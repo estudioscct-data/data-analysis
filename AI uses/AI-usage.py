2d73fd4eb4e334324b84f75518ff721d016efdc3
Carolina Caballero <estudioscct@gmail.com>
2026-05-31 01:00:30 -0400

    Add analysis of AI impact on student grades, including improvement metrics and tool usage

--- AI uses/AI-usage.py
+++ AI uses/AI-usage.py
@@ -46,18 +46,46 @@ plt.ylabel('Horas diarias en pantalla (promedio)', fontsize=12)
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.show()
 # the students who use IA improve their grades?
-data_ia = data[data['uses_ai'] == 'Yes'].copy()
+data_ia = data[data['uses_ai'] == 'Yes'].copy()[['grades_before_ai', 'grades_after_ai', 'purpose_of_ai', 'ai_tools_used']]
 data_ia['improvement'] = data_ia['grades_after_ai'] - data_ia['grades_before_ai']
 print(data_ia[['grades_before_ai', 'grades_after_ai', 'improvement']].head())
 mean_improvement = data_ia['improvement'].mean()
 print(f'Promedio de mejora en calificaciones: {mean_improvement:.2f}')
 print(f"Mediana de la mejora: {data_ia['improvement'].median():.2f} puntos")
-
-# Histograma de la diferencia 
 plt.hist(data_ia['improvement'], bins=10, edgecolor='black')
 plt.title('Distribución de la mejora en notas (después - antes)')
 plt.xlabel('Diferencia en puntos')
 plt.ylabel('Cantidad de estudiantes')
 plt.axvline(x=0, color='red', linestyle='--', label='Sin cambio')
 plt.legend()
+plt.show()
+#the purpose of using IA and their improvements are related?
+
+improve_purpose = data_ia.groupby('purpose_of_ai')['improvement'].mean().reset_index()
+improve_purpose.plot(kind='barh', x='purpose_of_ai', y='improvement', legend=False, color='teal', edgecolor='black')
+plt.title('Mejora promedio en notas según propósito de uso de IA')
+plt.xlabel('Mejora en puntos (después - antes)')
+plt.ylabel('Propósito')
+plt.axvline(x=0, color='red', linestyle='--')
+plt.show()
+
+#which AI tools are more used by students? and which one is the most effective in improving their grades?
+most_popular = data_ia['ai_tools_used'].value_counts()
+print("Herramientas de IA más utilizadas por los estudiantes:")
+print(most_popular)
+most_popular.plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Herramientas de IA más utilizadas por los estudiantes')
+plt.xlabel('Herramienta de IA')
+plt.ylabel('Cantidad de estudiantes')
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)   
+plt.show()
+
+most_effective = data_ia.groupby('ai_tools_used')['improvement'].mean().reset_index()
+most_effective = most_effective.sort_values(by='improvement', ascending=False)
+most_effective.plot(kind='barh', x='ai_tools_used', y='improvement', legend=False, color='coral', edgecolor='black')
+plt.title('Mejora promedio en notas según herramienta de IA utilizada')
+plt.xlabel('Mejora en puntos (después - antes)')
+plt.ylabel('Herramienta de IA')
+plt.axvline(x=0, color='red', linestyle='--')
 plt.show()
